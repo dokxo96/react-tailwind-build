dded72beca075614b3a658f15afeb062832caf51
dokxo <danielmora@cloudmex.io>
2021-08-01 14:15:31 -0600

    login2

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
                 <Switch>
                     <Route exact path="/" component={landing} />
                     <Route exact path="/login" component={Login} />
+                    <Route exact path="/login2" component={Login} />
                     <Route exact path="/register" component={Register} />
 
                     <Route exact path="/dashboard" component={Dashboard} />
