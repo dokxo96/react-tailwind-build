e429ad88ea344333f3cdaf1863a750c50deee9e7
dokxo <danielmora@cloudmex.io>
2021-08-01 09:30:12 -0600

    filesij

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-import { Switch, Route, Redirect } from 'react-router-dom';
+ import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 import Register from './pages/Register';
 
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,10 +1,4 @@
-import Card from '@material-tailwind/react/Card';
-import CardHeader from '@material-tailwind/react/CardHeader';
-import CardBody from '@material-tailwind/react/CardBody';
-import CardFooter from '@material-tailwind/react/CardFooter';
-import H5 from '@material-tailwind/react/Heading5';
-import InputIcon from '@material-tailwind/react/InputIcon';
-import Button from '@material-tailwind/react/Button';
+ 
 
 export default function Register() {
     return (
