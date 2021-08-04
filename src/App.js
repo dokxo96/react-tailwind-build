cc1b8601f301762ad4f284765d55e1f8660eaf87
dokxo96 <dokxo96@gmail.com>
2021-08-03 19:00:35 -0600

    setup for netlify

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import Sidebar from 'components/Sidebar';
+import Dashboard from 'pages/Dashboard';
+import Settings from 'pages/Settings';
+import Tables from 'pages/Tables';
+import Maps from 'pages/Maps';
+import Footer from 'components/Footer';
+import landing from 'pages/Landing';
+import profile from 'pages/Profile';
+import Login from 'pages/Login';
+import Register from 'pages/Register';
+import not404 from 'pages/not404';
+import Grupos  from 'pages/Ventas/Capturas/Grupos';
+import Subgrupos  from 'pages/Ventas/Capturas/Subgrupos';
+import Productos  from 'pages/Ventas/Capturas/Productos_sat';
+import Unidad_de_medida from 'pages/Ventas/Capturas/Unidad_de_medida_sat';
+import Rutas from 'pages/Ventas/Catalogos/Rutas';
+import Clientes from 'pages/Ventas/Catalogos/Clientes';
+ // Tailwind CSS Style Sheet
+import 'assets/styles/tailwind.css';
 
-import Register from './pages/Register';
-
-function App() {
-  return (
-    <>
+ function App() {
+    return (
+        <>
             
-    <div  >
-        <Switch>
-         
-            <Route exact path="/" component={Register} />
+            <div  >
+                <Switch>
+                    <Route exact path="/" component={Login} />
+                    <Route exact path="/register" component={Register} />
+
+                    <Route exact path="/dashboard" component={Dashboard} />
+                    <Route exact path="/settings" component={Settings} />
+                    <Route exact path="/tables" component={Tables} />
+                    <Route exact path="/Ventas/Capturas/Grupos" component={Grupos} />
+                    <Route exact path="/Ventas/Capturas/Subgrupos" component={Subgrupos} />
+                    <Route exact path="/Ventas/Capturas/Productos" component={Productos} />
  
-        </Switch>
-        
-    </div>
-</>
-);
+                    <Route exact path="/Ventas/Capturas/Unidad_de_medida" component={Unidad_de_medida} />
+                    <Route exact path="/Ventas/Catalogos/Rutas" component={Rutas} />
+                    <Route exact path="/Ventas/Catalogos/Clientes" component={Clientes} />
+
+                    <Route exact path="/profile" component={profile} />
+                    <Route  from="/notfound" component={not404} />
+                    <Redirect from="*" to="/notfound" />
+                </Switch>
+               
+            </div>
+        </>
+    );
 }
 
 export default App;
